02b2d5494b13dfede57a0d4236a2a7822634cdde
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-03 14:03:13 +0200

    Change second function as arrow function

--- functions/arrowFunctions.js
+++ functions/arrowFunctions.js
@@ -1,18 +1,24 @@
+// ---------------------------------------- 1)
 function sum(a, b) {
     return a + b;
 }
 
 let sum2 = (a, b) => a + b;
 
-
+// ---------------------------------------- 2)
 function isPositive(number) {
     return number >= 0;
 }
 
+let isPositive2 = number => number >= 0;
+let isPositive3 = () => number >= 0;
+
+// ---------------------------------------- 3)
 function randomNumber() {
     return Math.random;
 }
 
+// ---------------------------------------- 4)
 document.addEventListener('click', function() {
     console.log('Clicked');
 })
