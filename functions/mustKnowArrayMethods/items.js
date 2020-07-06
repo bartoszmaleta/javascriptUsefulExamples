4bde9cabce4b7d7329179d9304c15bdeb4b1bfca
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-06 05:14:28 +0200

    Add array.filter()

---
+++ functions/mustKnowArrayMethods/items.js
@@ -0,0 +1,19 @@
+// https://www.youtube.com/watch?v=R8rmfD9Y5-c&t=2s
+// 8 Must Know JavaScript Array Methods
+
+const items = [
+    { name: 'Bike',        price: 100},
+    { name: 'TV',        price: 200},
+    { name: 'Album',        price: 10},
+    { name: 'Book',        price: 5},
+    { name: 'Phone',        price: 500},
+    { name: 'Computer',        price: 1000},
+    { name: 'Keyboard',        price: 25}
+]
+
+const filteredItems = items.filter((item) => {
+    return item.price <= 100;
+})
+
+console.log(filteredItems);
+
